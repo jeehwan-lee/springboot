c538d45dfffc17c64a7799f3c06b46a2d3e480aa
이지환 사원 개발혁신센터 데브옵스파트 (jeehwan_lee@biztechi.com <jeehwan_lee@cns.lgcns.com>
2023-06-21 17:43:08 +0900

    first commit

---
+++ src/main/java/com/example/springboot3/Springboot3Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3Application {
+	// https://congsong.tistory.com/15
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3Application.class, args);
+	}
+
+}
